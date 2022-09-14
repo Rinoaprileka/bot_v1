dbc193659bb6b13bfb6e6651fdfc8dd36b175677
Rinoaprileka <113507707+Rinoaprileka@users.noreply.github.com>
2022-09-15 02:29:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,9 +47,9 @@ global.psaweria = 'https://saweria.com/mrw4h1d'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '6282159957081'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorown = '6281358542914'
+global.namebot = 'RINN-BOTX'
+global.nameown = 'RINO-APRILEKA'
 
 /*============== APIKEY SIMPEL ==============*/
 global.zenzkey = 'b8040941f7'
@@ -58,9 +58,9 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127487538', 'WH MODS DEV', true],
-  ['6282159957081', 'WH-MODS-BOT-V1', true],
-  ['6288298925386', 'WH-MODS-BOT-V2', true],
+  ['6281358542914', 'RINO-APRILEKA', true],
+  ['6281358542914', 'WH-MODS-BOT-V1', true],
+  ['6281358542914', 'WH-MODS-BOT-V2', true],
   ['79917357842', 'WH-MODS-BOT-V3', true] //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
